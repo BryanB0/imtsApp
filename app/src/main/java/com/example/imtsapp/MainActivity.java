8d464b2a9da521423d45c0a25f8b1903eb4bd0c7
Bryan1675 <bosseb6@gmail.com>
2024-01-29 17:09:38 +0100

    Some change

--- app/src/main/java/com/example/imtsapp/MainActivity.java
+++ app/src/main/java/com/example/imtsapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.imtsapp;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private TextView welcomeMessageTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.welcomeMessageTextView = findViewById(R.id.welcome_message);
+        this.welcomeMessageTextView.setText(
+                getResources().getText(R.string.HelloWorld) + " Solacroup"
+        );
     }
 }
